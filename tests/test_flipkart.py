04c7e7e8db5854490542f2f3d58ca84429fef2e7
Inderqa <79741516+Inderqa@users.noreply.github.com>
2026-05-17 00:10:01 +0530

    Commented Full test flipkar file

--- tests/test_flipkart.py
+++ tests/test_flipkart.py
@@ -1,10 +1,10 @@
-import pytest
-from pytest_playwright.pytest_playwright import browser, playwright, context
+# import pytest
+# from pytest_playwright.pytest_playwright import browser, playwright, context
 
-from pageObject.flipkart import *
-from playwright.sync_api import Page
+# from pageObject.flipkart import *
+# from playwright.sync_api import Page
 
-from utilities.base_class_utils import BaseClass
+# from utilities.base_class_utils import BaseClass
 
 @pytest.mark.usefixtures("start_browser_with_tracing")
 # def test_flipkart_aut(start_browser_with_tracing):
@@ -21,10 +21,3 @@ from utilities.base_class_utils import BaseClass
 #     base.go_to_url("https://www.flipkart.com")
 #     flipkart = flipkartHome(page)
 #     flipkart.flipkart_mobile_name_price_in_ascending_order("Redmi 5A (Blue, 16 GB)")
-
-
-
-
-
-
-
