95df07a936fc7724bdd1d67576e9fd30950f1ffe
Inderqa <79741516+Inderqa@users.noreply.github.com>
2026-05-16 23:59:51 +0530

    Update test_csv_Data_read.py

--- tests/test_csv_Data_read.py
+++ tests/test_csv_Data_read.py
@@ -1,8 +1,8 @@
 import csv
-
-
+from pathlib import Path
+csv_path = Path(__file__).parent.parent / "test_results.csv"
 def test_read_csv_data():
-    with open("/Users/inderpreetsingh/PycharmProjects/Playwrightpythonproject_RS/test_results.csv",newline='') as file:
+    with open(csv_path,newline='') as file:
         reader = csv.DictReader(file)
         for row in reader:
             if row['Status'] == "Failed" or row['Status'] == "Skipped":
