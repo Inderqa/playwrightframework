a49f7d2fb8b6f7d67679e4513e712659ab2bfd8b
Inderqa <79741516+Inderqa@users.noreply.github.com>
2026-05-17 00:05:32 +0530

    Commented tests for flipkart later can uncomment them

--- tests/test_flipkart.py
+++ tests/test_flipkart.py
@@ -7,20 +7,20 @@ from playwright.sync_api import Page
 from utilities.base_class_utils import BaseClass
 
 @pytest.mark.usefixtures("start_browser_with_tracing")
-def test_flipkart_aut(start_browser_with_tracing):
-    page = start_browser_with_tracing
-    base = BaseClass(page)
-    base.go_to_url("https://www.flipkart.com/")
-    flipkart = flipkartHome(page)
-    flipkart.flipkart_get_mobile_price_with_mobile_name("Redmi 5A (Blue, 16 GB)")
+# def test_flipkart_aut(start_browser_with_tracing):
+#     page = start_browser_with_tracing
+#     base = BaseClass(page)
+#     base.go_to_url("https://www.flipkart.com/")
+#     flipkart = flipkartHome(page)
+#     flipkart.flipkart_get_mobile_price_with_mobile_name("Redmi 5A (Blue, 16 GB)")
 
 
-def test_product_name_price_sorted(start_browser_with_tracing):
-    page = start_browser_with_tracing
-    base = BaseClass(page)
-    base.go_to_url("https://www.flipkart.com")
-    flipkart = flipkartHome(page)
-    flipkart.flipkart_mobile_name_price_in_ascending_order("Redmi 5A (Blue, 16 GB)")
+# def test_product_name_price_sorted(start_browser_with_tracing):
+#     page = start_browser_with_tracing
+#     base = BaseClass(page)
+#     base.go_to_url("https://www.flipkart.com")
+#     flipkart = flipkartHome(page)
+#     flipkart.flipkart_mobile_name_price_in_ascending_order("Redmi 5A (Blue, 16 GB)")
 
 
 
